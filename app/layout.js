4e307e139ea79e144c9fbaf4499e25aca89267fe
Yuvraj <geniusandsmart123@gmail.com>
2026-06-14 09:19:39 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,22 @@
-
 import Navbar from "./components/Navbar";
 import { ToastContainer, Slide } from "react-toastify";
 
 import "./globals.css";
 export const metadata = {
   title: "Bubble / Instant Chat ",
-  description: "A service that let you chat to your friends without having to login or create account.",
-  verification: {
-    google: "google6303c4a18fefe718.html",
-  },
-  other: {
-    "google-adsense-account": "ca-pub-8180810397719502",
-  }
+  description:
+    "A service that let you chat to your friends without having to login or create account.",
 };
 
 export default function RootLayout({ children }) {
-
   return (
-   <html lang="en" className="bg-[#05644b]">
-      <body className="h-screen">
+    <html lang="en" className=" bg-[#05644b] h-full">
+      <body className="h-full flex flex-col">
         <Navbar />
         <ToastContainer
           position="top-right"
-           autoClose={false}
+          autoClose={false}
           hideProgressBar={true}
-           
-          
           newestOnTop={false}
           closeOnClick
           rtl={false}
@@ -35,8 +26,8 @@ export default function RootLayout({ children }) {
           theme="dark"
           transition={Slide}
         />
-        {children}
+        <main className="flex-1 min-h-0">{children}</main>
       </body>
-    </html >
+    </html>
   );
 }
